2d254bc89c587b835b4dec9244408de5138816c6
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-08-28 18:21:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch")
 
 setInterval(() => {
   const random = String(Math.random() * 10).replace(".","")
-  const url = "https://github.com/Rednexie/is.gd" + random;
+  const url = "https://github.com/Rednexie/is.gd?" + random;
   fetch("https://is.gd/create.php?format=simple&url=" + url)
     .then(res => res.text())
     .then(data => console.log(data))
