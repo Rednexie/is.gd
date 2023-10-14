8ef464f33c595612cad41f6ca85f3d7c4ffab825
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-10-14 19:59:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-const cluster = require("cluster")
-const main = cluster.isMaster
-const cores = 5;
+const cluster = require('cluster');
+const main = cluster.isMaster;
+const cores = 10;
 
 if (main) {
   for (let i = 0; i < cores; i++) {
-    cluster.fork()
+    cluster.fork();
   }
   cluster.on('exit', (worker, code, sig) => {
-    console.log("The Thread with the " + worker.process.pid + "exited with the code: " + code)
+    console.log(
+      'The Thread with the ' +
+      worker.process.pid +
+      'exited with the code: ' +
+      code
+    );
   });
-}
-else {
-  console.log("sa")
+} else {
+  console.log(process.pid);
   setInterval(() => {
-    const random = String(Math.random() * 10).replace(".", "")
-    const url = "https://github.com/Rednexie/is.gd?" + random;
-    fetch("https://is.gd/create.php?format=simple&url=" + url)
-      .then(res => res.text())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
-  }, 1)
+    const random = String(Math.random() * 10).replace('.', '');
+    const url = 'https://github.com/Rednexie/is.gd?' + random;
+    fetch('https://is.gd/create.php?format=simple&url=' + url)
+      .then((res) => res.text())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  }, 1);
 }
