89fd8f1c1f04323ba21834d1db2cbfdc57efe429
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-10-14 19:43:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const fetch = require("node-fetch") // npm install node-fetch@2.6.1
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const cluster = require("cluster")
+const main = cluster.isMaster
+const cores = 5;
 
-setInterval(() => {
-  const random = String(Math.random() * 10).replace(".","")
-  const url = "https://github.com/Rednexie/is.gd?" + random;
-  fetch("https://is.gd/create.php?format=simple&url=" + url)
-    .then(res => res.text())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-},1)
+if (main) {
+  for (let i = 0; i < cores; i++) {
+    cluster.fork()
+  }
+  cluster.on('exit', (worker, code, sig) => {
+    console.log("The Thread with the " + worker.process.pid + "exited with the code: " + code)
+  });
+}
+else {
+  console.log("sa")
+  setInterval(() => {
+    const random = String(Math.random() * 10).replace(".", "")
+    const url = "https://github.com/Rednexie/is.gd?" + random;
+    fetch("https://is.gd/create.php?format=simple&url=" + url)
+      .then(res => res.text())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }, 1)
+}
