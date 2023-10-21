9ec9a86f0e44136f03cc19def0b405c4ba385b3c
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-10-21 14:51:40 +0300

    Create single.js

---
+++ single.js
@@ -0,0 +1,11 @@
+const request = require("request")
+setInterval(() => {
+  const random = String(Math.random() * 10).replace(".","")
+  const url = "https://github.com/Rednexie/is.gd?" + random;
+  request({
+    url: "https://is.gd/create.php?format=simple&url=" + url,
+    json: true
+  }, (err, res, body) => {
+    console.log(body)
+  })
+},50)
