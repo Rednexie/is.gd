9e28a2902b88d08c425304695247d626f387f996
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-10-21 14:51:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-const request = require("request")
-setInterval(() => {
-  const random = String(Math.random() * 10).replace(".","")
-  const url = "https://github.com/Rednexie/is.gd?" + random;
-  request({
-    url: "https://is.gd/create.php?format=simple&url=" + url,
-    json: true
-  }, (err, res, body) => {
-    console.log(body)
-  })
-},50)
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const cluster = require('cluster');
+const main = cluster.isMaster;
+let { threads, delay} = require("./config.json");
+
+if(typeof threads !== "number") threads = Number(threads);
+if(typeof delay !== "number") delay = Number(delay);
+if(isNaN(threads) || threads === 0){
+  console.log("threads should be a number greater than 0, edit config.json")
+  return process.exit(1)
+}
+if(isNaN(delay)){
+  console.log("delay should be a number, edit config.json")
+  return process.exit(1)
+}
+
+
+
+if (main) {
+  
+  for (let i = 0; i < threads; i++) {
+    const worker = cluster.fork({id: i + 1});
+    worker.on('message', (msg) => {
+      console.log(msg)
+    })
+  }
+  cluster.on('exit', (worker, code, sig) => {
+    console.error(
+      'The Thread with the ' +
+      worker.process.pid +
+      'exited with the code: ' +
+      code
+    );
+  });
+
+
+} else {
+  console.log(`Worker with the process id: ${process.pid} and id: ${process.env.id} has been started.`);
+  setInterval(() => {
+    const random = String(Math.random() * 10).replace('.', '');
+    const url = 'https://github.com/Rednexie/is.gd?' + random;
+    fetch('https://is.gd/create.php?format=simple&url=' + url)
+      .then((res) => res.text())
+      .then((data) => process.send(data))
+      .catch((error) => process.send(error));
+  }, delay);
+}
